b28a7d7b1721d0209e5f14e187a2db09c05025d7
irfan-fauzi <irfanwintech@yahoo.com>
2020-03-13 22:25:44 +0700

    constructor.js

--- function-declaration.js
+++ function-declaration.js
@@ -1,14 +1,14 @@
 // object dengan teknik function declaration
 function createObjectStaff(name, position, nip, salary) {
-    //buat object kosong untuk kemudian diisi
-    let staff = {};
-    //isi parameter yang akan digunakan
-    staff.name = name;
-    staff.position = position;
-    staff.nip = nip;
-    staff.salary = salary;
-    // panggil kembali objectnya
-    return staff;
+  //buat object kosong untuk kemudian diisi
+  let staff = {};
+  //isi parameter yang akan digunakan
+  staff.name = name;
+  staff.position = position;
+  staff.nip = nip;
+  staff.salary = salary;
+  // panggil kembali objectnya
+  return staff;
 }
 // isi value (argumen) kedalam parameter object nya
 // misal contoh ada data staff baru
